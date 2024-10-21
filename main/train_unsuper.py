03473c252a86840c29fa1ff5573a7e8ccbbb4bb8
neminwu <neminwu@gmail.com>
2024-10-21 22:14:48 +0000

    add tutorial

--- main/train_unsuper.py
+++ main/train_unsuper.py
@@ -1,30 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-import os
-import torch
-import pickle
-from argparse import ArgumentParser
-from torch import optim
-import models
-import utils as ut
-import datasets as dt
-import data_utils as dtul
-import grid_predictor as grid
-from paths import get_paths
-import losses as lo
-
 from dataloader import *
 from trainer_helper import *
 from eval_helper import *
 from trainer import *
 
-parser = make_args_parser()
-args = parser.parse_args()
+def main():
+    # Argument parsing
+    parser = make_args_parser()
+    args = parser.parse_args()
 
-trainer = Trainer(args, console=True)
-trainer.run_train()
-trainer.run_eval_final()
+    # Initialize the Trainer
+    trainer = Trainer(args, console=True)
 
-val_preds = trainer.run_eval_spa_enc_only(
-    eval_flag_str="LocEnc ", load_model=True)
+    # Run training
+    trainer.run_train()
+
+    # Run final evaluation
+    trainer.run_eval_final()
+
+    val_preds = trainer.run_eval_spa_enc_only(eval_flag_str="LocEnc ", load_model=True)
+
+if __name__ == "__main__":
+    main()
