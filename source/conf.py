0e562327a7f70e80e8b481296f05a4ee97f754d3
Qian Cao <144297473+QianC95@users.noreply.github.com>
2024-10-21 01:01:04 -0400

    from sphinx.ext import imgconverter

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 #import sphinx_pdj_theme
+from sphinx.ext import imgconverter
 
 # -- Project information -----------------------------------------------------
 
